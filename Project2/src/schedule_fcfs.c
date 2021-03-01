aa7e1db298531332886d61ff22281ee513747cb0
Reuben Ninan <intelphysic@gmail.com>
2021-02-28 22:02:01 -0700

    completed fcfs methods

--- Project2/src/schedule_fcfs.c
+++ Project2/src/schedule_fcfs.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "schedulers.h"
 #include "list.h"
 #include "cpu.h"
@@ -7,23 +8,57 @@ struct node *head = NULL;
 
 int tid = 0;
 
+void reverseList()
+{
+    struct node *prevNode, *curNode;
+
+    if (head != NULL)
+    {
+        prevNode = head;
+        curNode = head->next;
+        head = head->next;
+
+        prevNode->next = NULL;
+
+        while (head != NULL)
+        {
+            head = head->next;
+            curNode->next = prevNode;
+
+            prevNode = curNode;
+            curNode = head;
+        }
+
+        head = prevNode;
+    }
+}
+
 void schedule()
 {
+    reverseList();
+
+    struct node *temp;
+    temp = head;
+
+    while (temp != NULL)
+    {
+        run(temp->task, temp->task->burst);
+        temp = temp->next;
+    }
 }
 
 void add(char *name, int priority, int burst)
 {
-    Task *task = {
-        name,
-        priority,
-        burst};
+    Task task = {
+        .name = name,
+        .priority = priority,
+        .burst = burst,
+        .tid = tid};
 
-    printf("[%s] [%d] [%d]\n", task->name, task->priority, task->burst);
+    Task *task_ptr = &task;
+    Task *cpy = malloc(sizeof(task_ptr));
+    *cpy = *task_ptr;
+
+    insert(&head, cpy);
     tid++;
-    // insert(&head, task);
-
-    // if (tid == 5)
-    // {
-    //     traverse(head);
-    // }
 }
